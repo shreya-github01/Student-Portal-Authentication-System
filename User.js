7715514df8a513bba0b0e802b0f7e54cad63cb70
Shreya Shrivastava <136108100+shreya-github01@users.noreply.github.com>
2026-05-02 15:44:19 +0530

    Added User.js

---
+++ User.js
@@ -0,0 +1,7 @@
+const mongoose = require("mongoose");
+
+module.exports = mongoose.model("User", new mongoose.Schema({
+  name:     String,
+  email:    { type: String, unique: true },
+  password: String,
+}));
